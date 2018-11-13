129801566eef4702de9c548eb3f944d0fdb5e87d
zhangdian <zhangdian@inspur.com>
2018-11-13 16:34:04 +0800

    zhang dian update bean and redfish util and log

---
+++ ipodm/ipodm-api/src/main/java/com/inspur/podm/business/services/redfish/ActionService.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016-2018 inspur Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.inspur.podm.business.services.redfish;
+
+import java.util.concurrent.TimeoutException;
+
+import com.inspur.podm.business.BusinessApiException;
+import com.inspur.podm.business.dto.actions.actionInfo.ActionInfoDto;
+import com.inspur.podm.business.services.context.Context;
+import com.inspur.podm.common.types.redfish.RedfishAction;
+
+public interface ActionService<T extends RedfishAction> {
+    void perform(Context target, T request) throws BusinessApiException, TimeoutException;
+    default ActionInfoDto getActionInfo(Context target) throws BusinessApiException {
+        throw new BusinessApiException("Operation not supported");
+    }
+}
